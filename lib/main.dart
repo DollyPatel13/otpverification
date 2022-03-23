59ada85506011babcddd1cb611c6763143517e48
Dolly Patel <dollypatel280501@gmail.com>
2022-03-23 17:14:29 +0530

    OTP Verification with firebase

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:beautyparlorapp/screens/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:beautyparlorapp/screens/login_screen.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await  Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      debugShowCheckedModeBanner: false,
+      home:InitializerWidget(),
+    );
+  }
+}
+class InitializerWidget extends StatefulWidget {
+  @override
+  _InitializerWidgetState createState() => _InitializerWidgetState();
+}
+
+class _InitializerWidgetState extends State<InitializerWidget> {
+
+  late FirebaseAuth _auth;
+
+  late User? _user;
+
+  bool isLoading = true;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _auth = FirebaseAuth.instance;
+    _user = _auth.currentUser;
+    isLoading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading ? Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    ) : _user== null ? LoginScreen() : HomeScreen();
+  }
+}
